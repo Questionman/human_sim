4ee47eab77805f272ee18910802378e28a3ed447
jihong <diti94@naver.com>
2018-12-05 10:43:53 +0900

    Fixed cached files for gitignore

--- build/CMakeFiles/human_phantom.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/human_phantom"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/human_phantom.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
